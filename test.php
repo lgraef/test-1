51f5083f2630e1379cfa3a75a92997ecb3a9a32b
Lorenz Graef <lorenz.graef@startplatz.de>
2013-03-31 11:18:13 +0200

    First commit

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: graef
+ * Date: 31.03.13
+ * Time: 11:16
+ * To change this template use File | Settings | File Templates.
+ */
+$test = "eins";
+$zwei = "zwei";
